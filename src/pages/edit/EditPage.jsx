980ac403f5b25c523bef86ad80704e746b0461fc
bluepaper14 <skykong14@gmail.com>
2025-11-03 17:44:06 +0900

    feat: 변경 테스트

--- src/pages/edit/EditPage.jsx
+++ src/pages/edit/EditPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 function EditPage() {
+   // 수정용 주석
+   //  변경하기
   return (
     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
       <h1>Edit Page</h1>
+      <p>테스트용 초기 페이지</p>
       <p>테스트용 초기 페이지</p>
     </div>
   );
