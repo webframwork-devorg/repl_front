02a7b92b368b4c9de95b6fbcc5aa89b8b0f2c277
silverzzo <124555750+silverzzo@users.noreply.github.com>
2025-11-08 20:51:44 +0900

    fix: 북마크 디테일 수정

--- src/components/commons/bookmarks/BookMark.jsx
+++ src/components/commons/bookmarks/BookMark.jsx
@@ -1,23 +1,49 @@
 import React from "react";
-import bookmarkBackground from "@/assets/images/bookmark1.svg";
+
+import blueBg from "@/assets/images/bookmark-blue.svg";
+import noteBg from "@/assets/images/bookmark-note.svg";
+import pinkBg from "@/assets/images/bookmark-pink.svg";
+import OrangeBg from "@/assets/images/bookmark-orange.svg";
+import whiteBg from "@/assets/images/bookmark-white.svg";
+
+const bookmarkBackgrounds = {
+
+  1: whiteBg,   // ID 1번: 흰색
+  4: blueBg,    // ID 4번: 파란색
+  2: pinkBg,     // ID 2번: 분홍색
+  3: OrangeBg,  // ID 3번: 주황색
+  5: noteBg,    // ID 5번: 노트배경
+};
+
 /**
  * @param {object} props - 컴포넌트 props
  * @param {string} props.text - 책갈피로 표시될 텍스트
+ * @param {string} props.pageNumber - 페이지 번호
+ * @param {number} props.backgroundId - 배경 ID (1, 2, 3...)
  */
+function BookMark({ text, pageNumber, backgroundId }) { 
 
-
-function BookMark({ text }) {
+    //기본값 1번(파란색)
+    const selectedBg = bookmarkBackgrounds[backgroundId] || bookmarkBackgrounds[1];
 
     const bookmarkStyle= {
-        backgroundImage: `url(${bookmarkBackground})`,
+        backgroundImage: `url(${selectedBg})`, 
     };
-     const baseStyle = `mx-4 w-90 h-25 bg-no-repeat bg-contain bg-center flex items-center justify-center p-5 text-black font-bold text-[13px] text-center whitespace-pre-wrap break-keep`;
+
+    const baseStyle = `mx-4 w-90 h-25 bg-no-repeat bg-contain bg-center flex items-center justify-start p-5 text-black font-bold text-[13px] text-start whitespace-pre-wrap break-keep`;
+    
     return (
         <div className={baseStyle} style={bookmarkStyle}>
-            <p>"{text}"</p>
+            <div>
+                {pageNumber && (
+                    <p className="text-[14px] font-medium opacity-80 mb-1">
+                        {pageNumber}P
+                    </p>
+                )}
+                <p>"{text}"</p>
+            </div>
         </div>
     )
 }
 
 export default BookMark;
-
