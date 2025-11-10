5a2ca45451d18558aaf9e190f5bc3de54023d5f6
yunnnn <goldd951@naver.com>
2025-11-10 13:29:16 +0900

    feat: 하트 및 공유 버튼 컴포넌트 중간 개발

---
+++ src/components/commons/buttons/HeartShareButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+/**
+// 하트 버튼
+// 공유 버튼
+
+@param {object} props - 컴포넌트 props
+@param {React.ReactNode} props.children - 버튼 안에 들어갈 내용
+@param {"black" | "white"} [props.variant="black"] - 버튼 스타일 (기본값: black)
+@param {function} [props.onClick] - 버튼 클릭 이벤트 핸들러
+*/
+
+function HeartShareButton({ children, variant = "black", onClick }) {
+  const baseStyle = "w-full h-full rounded-[15px] text-[16px]";
+
+  const variants = {
+    black: "bg-black text-white font-bold hover:bg-gray-800",
+    white:
+      "bg-white text-black font-bold font-inter",
+  };
+
+  return (
+    <button className={`${baseStyle} ${variants[variant]}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default BasicButton;
